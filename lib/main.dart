f847466fc6cd8f7833008b8e23d600b5d2a1272b
hwpl-bit <office.hwplindia@gmail.com>
2023-07-08 11:51:18 +0530

    challenge app

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,25 @@ import 'package:get/get.dart';
 import 'package:taskdemo/module/view/challenge_screen.dart';
 
 main() {
-  runApp(GetMaterialApp(debugShowCheckedModeBanner:false,home: ChallengeScreen()));
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Challenge App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // home: const MyHomePageNew(title: 'Flutter Demo Home Page'),
+      home:ChallengeScreen(),
+    );
+  }
+}
